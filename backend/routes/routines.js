16682d00a9e646c481f0279ed7ab8ecd3e950a83
Ilayda <ilaydasungur@gmail.com>
2025-09-20 07:10:39 +0300

    Your commit message

--- backend/routes/routines.js
+++ backend/routes/routines.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const axios = require('axios')
-const supabase = require('../services/supabaseClient')
+const { supabase } = require('../services/supabaseClient')
 
 const router = express.Router()
 
